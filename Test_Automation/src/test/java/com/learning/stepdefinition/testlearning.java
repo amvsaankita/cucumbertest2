7d1321377206098804e7bba1f209a69178981d06
amvsaankita <amvsa.ankita@gmail.com>
2019-06-06 22:27:06 +0000

    my third  code

--- Test_Automation/src/test/java/com/learning/stepdefinition/testlearning.java
+++ Test_Automation/src/test/java/com/learning/stepdefinition/testlearning.java
@@ -1,20 +1,30 @@
 package com.learning.stepdefinition;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class testlearning {
+	
+	public WebDriver driver;
+
 	@Given("^user is  on homepage$")
 	public void user_is_on_homepage() throws Throwable {
-	    System.out.println("hello");
-	}
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankita\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		 driver= new ChromeDriver();	}
 
 	@When("^user navigates to Login Page$")
 	public void user_navigates_to_Login_Page() throws Throwable {
-	    System.out.println("hello");
-
+		driver.get("https://www.google.co.in/?gws_rd=ssl");
+		String str= driver.getTitle();
+		System.out.println(str);
+		Thread.sleep(3000);
+		
 	}
 
 	@When("^user enters username and Password$")
